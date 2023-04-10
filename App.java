c10515674ce10f7e8ff6d2af17bf699ee50b57c5
Heloisa dos Santos <heloisadossantos010@gmail.com>
2023-04-10 16:09:40 -0300

    exercicioWhile

--- App.java
+++ App.java
@@ -5,10 +5,11 @@ public class App {
 
     public static void main(String[] args) {
         exercicioWhile obj= new exercicioWhile();
-        //obj.exercicio1();
-        //obj.exercicio2();
-        //obj.exercicio3();
-        //obj.exercicio4();
+        obj.exercicio1();
+        obj.exercicio2();
+        obj.exercicio3();
+        obj.exercicio4();
         obj.exercicio5();
+        obj.exercicio6();
     }
 }
